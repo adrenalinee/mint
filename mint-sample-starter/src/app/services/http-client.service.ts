086624cc0250d95617d63731e18ae80804462927
dsshin <dsshin@coupang.com>
2017-06-02 22:36:18 +0900

    http request 에서 200 이외의 응답도 처리할 수 있도록 수정

--- mint-sample-starter/src/app/services/http-client.service.ts
+++ mint-sample-starter/src/app/services/http-client.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 
 import { RequestInfo, ResponseInfo, NameValue } from '../requestInfo';
 
@@ -34,45 +34,60 @@ export class HttpClientService {
           responseInfo.headers.push(new NameValue(k, response.headers.get(k)));
         });
 
-        responseInfo.body = response.text() == "" ? null : response.text();
+        responseInfo.body = response.text() == '' ? null : response.text();
 
         return responseInfo;
-      });
+      })
+      .catch(this.handleError);
   }
 
-  execute(request: RequestInfo):  Observable<ResponseInfo> {
-    let headers: Headers = new Headers();
-    request.headers
-    .filter(h => h.name != null && h.value != null)
-    .forEach(h => {
-      headers.set(h.name, h.value);
+  handleError(response: Response | any): Observable<ResponseInfo> {
+    console.log(response);
+    const responseInfo: ResponseInfo = new ResponseInfo();
+    responseInfo.status = response.status;
+    responseInfo.statusText = response.statusText;
+
+    response.headers.keys().forEach(k => {
+      responseInfo.headers.push(new NameValue(k, response.headers.get(k)));
     });
 
-    let finalUrl: string = request.url;
-    let method = request.method;
-    let body = request.body;
-
-    let requestOptions = new RequestOptions({
-      headers: headers,
-      method: method,
-      body: body
-    });
-
-
-    return this.http.request(finalUrl, requestOptions)
-      .map(response => {
-        // console.log(response);
-        let responseInfo: ResponseInfo = new ResponseInfo();
-        responseInfo.status = response.status;
-        responseInfo.statusText = response.statusText;
-
-        response.headers.keys().forEach(k => {
-          responseInfo.headers.push(new NameValue(k, response.headers.get(k)));
-        });
-
-        responseInfo.body = response.text() == "" ? null : response.text();
-
-        return responseInfo;
-      });
+    responseInfo.body = response.text() == '' ? null : response.text();
+    return Observable.of(responseInfo);
   }
+
+  // execute(request: RequestInfo):  Observable<ResponseInfo> {
+  //   let headers: Headers = new Headers();
+  //   request.headers
+  //   .filter(h => h.name != null && h.value != null)
+  //   .forEach(h => {
+  //     headers.set(h.name, h.value);
+  //   });
+
+  //   let finalUrl: string = request.url;
+  //   let method = request.method;
+  //   let body = request.body;
+
+  //   let requestOptions = new RequestOptions({
+  //     headers: headers,
+  //     method: method,
+  //     body: body
+  //   });
+
+
+  //   return this.http.request(finalUrl, requestOptions)
+  //     .map(response => {
+  //       // console.log(response);
+  //       let responseInfo: ResponseInfo = new ResponseInfo();
+  //       responseInfo.status = response.status;
+  //       responseInfo.statusText = response.statusText;
+
+  //       response.headers.keys().forEach(k => {
+  //         responseInfo.headers.push(new NameValue(k, response.headers.get(k)));
+  //       });
+
+  //       responseInfo.body = response.text() == "" ? null : response.text();
+
+  //       return responseInfo;
+  //     });
+  // }
 }
