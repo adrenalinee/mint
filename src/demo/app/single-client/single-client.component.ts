9546deae8ec74d0b28b953b4e1f2c4214afa9841
adrenalinee <adrenalinee31@gmail.com>
2017-11-14 16:20:15 +0900

    request, response 부분 ui padding, layout 조정 body에 값이 있을때 표시하는 아이콘 변경및 버그 수정 응답 기다리는중 프로그래스가 안보이던 문제 수정

--- src/demo/app/single-client/single-client.component.ts
+++ src/demo/app/single-client/single-client.component.ts
@@ -4,7 +4,7 @@ import { RequestView } from '../../../mintClient/src/requestViews';
 @Component({
   selector: 'app-single-client',
   template: `
-<div style="padding: 1em">
+<div style="padding: 0.5em 1em 0.5em 1em">
     <app-http-client [requestView]="requestView" [requestExpansions]="requestExpansions"></app-http-client>
 </div>
   `
